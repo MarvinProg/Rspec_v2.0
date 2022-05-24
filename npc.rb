0c4bc3ec5798c328fb49ee030a6c15ca90571068
Artem Lomakin <Lomakin-as@mail.ru>
2022-05-24 19:19:49 +0300

    add: method info and up-down health

--- npc.rb
+++ npc.rb
@@ -1,2 +1,28 @@
 class Person 
+  def initialize (name_personage, health=90)
+    @name_personage = name_personage.capitalize
+    @health = health
+  end
+
+  def name 
+    name = @name_personage
+    puts name
+  end
+
+  def health_up 
+    @health += 10
+  end
+
+  def health_down
+    @health -= 10
+  end
+
+  def info_about_npc 
+    puts "Name personage: #{@name_personage}, health: #{@health}"
+  end
 end
+
+npc = Person.new('artem')
+npc.name
+npc.health_up
+npc.info_about_npc
