61cdb135b4f271def9fc88dbd12f4c8fcfeeb1f0
Artem Lomakin <Lomakin-as@mail.ru>
2022-06-14 19:52:54 +0300

    update: Refactoring test variable

--- npc_spec.rb
+++ npc_spec.rb
@@ -1,27 +1,23 @@
 require "./npc"
 
 describe Person do
-  it "Capitalize name" do
-    npc = Person.new('artem')
+  before do 
+    @npc = Person.new('artem')
+  end
 
-    expect(npc.name).to eq 'Artem'
+  it "Capitalize name" do
+    expect(@npc.name).to eq 'Artem'
   end
 
   it "Up health" do
-    npc = Person.new('artem')
-
-    expect(npc.health_up).to eq 100
+    expect(@npc.health_up).to eq 100
   end
 
   it "Down health" do 
-    npc = Person.new('artem')
-
     expect(npc.health_down).to eq 80
   end
 
   it "information on npc" do
-    npc = Person.new('artem')
-
-    expect(npc.info_about_npc).to eq 'Name personage: Artem, health: 90'
+    expect(@npc.info_about_npc).to eq 'Name personage: Artem, health: 90'
   end
 end
