94094f86b21cc13cb61afd37096fc98b773ffa11
Artem Lomakin <Lomakin-as@mail.ru>
2022-06-14 19:41:51 +0300

    feture: add information npc test

--- npc_spec.rb
+++ npc_spec.rb
@@ -12,4 +12,16 @@ describe Person do
 
     expect(npc.health_up).to eq 100
   end
+
+  it "Down health" do 
+    npc = Person.new('artem')
+
+    expect(npc.health_down).to eq 80
+  end
+
+  it "information on npc" do
+    npc = Person.new('artem')
+
+    expect(npc.info_about_npc).to eq 'Name personage: Artem, health: 90'
+  end
 end
